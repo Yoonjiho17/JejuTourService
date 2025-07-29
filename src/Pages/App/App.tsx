868660da76b2fad8e058ed24726bf11b34cd6af9
Yoon JiHo <dbswlgh0627@gamil.com>
2025-07-29 20:18:56 +0900

    refactor: RouterHandler파일삭제 후 App에서 버튼별 페이지 구현

--- src/Pages/App/App.tsx
+++ src/Pages/App/App.tsx
@@ -1,13 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "../MainPage/MainPage";
-import RouterHandler from "../RouterHandler";
+import Accommodation from "../Accommodation/Accommodation";
+import Attractions from "../Attractions/Attractions";
+import MarineLeisure from "../MarineLeisure/MarineLeisure";
+import SafetyFacilities from "../SafetyFacilities/SafetyFacilities";
+import ConvenienceFacilities from "../ConvenienceFacilities/ConvenienceFacilities";
+import SpecialResources from "../SpecialResources/SpecialResources";
+import ExcellentRestaurants from "../ExcellentRestaurants/ExcellentRestaurants";
+import TourPlanner from "../TourPlanner/TourPlanner";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/:page" element={<RouterHandler />} />
+        <Route path="/숙박업소" element={<Accommodation />} />
+        <Route path="/관광지" element={<Attractions />} />
+        <Route path="/해양레저스포츠" element={<MarineLeisure />} />
+        <Route path="/안전시설" element={<SafetyFacilities />} />
+        <Route path="/편의시설" element={<ConvenienceFacilities />} />
+        <Route path="/마을특화자원" element={<SpecialResources />} />
+        <Route path="/모범음식점" element={<ExcellentRestaurants />} />
+        <Route path="/여행일정계획" element={<TourPlanner />} />
+        <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
       </Routes>
     </Router>
   );
